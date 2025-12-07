be0e1eeabe60ab4ddc2d7d78b18ceb871bdd2613
KosukeNakagawa95 <kna0891@gmail.com>
2025-12-07 19:27:00 +0900

    refactor: Line28に()を追加 refactor: Line24のintを削除

--- future_choice.py
+++ future_choice.py
@@ -7,11 +7,24 @@ total_abundance = (human_productivity + (optimus_productivity * num_robots)) * 1
 print(f"Yeehaw! Optimusのおかげで豊かさレベル: {total_abundance}倍！ 働く？ オプションやで！🤠")
 
 # 未来の選択肢関数
-def future_choice():
-    print("オプション1: 働く（ボーナスGET）")
-    print("オプション2: 趣味三昧（ビール飲み放題）")
-    print("ワイのオススメ: 両方！")
+def future_map_one():
+    return ("オプション1: 働く（ボーナスGET）")
 
-future_choice()
-print("------------------------------------------")
-future_choice()
+def future_map_two():
+    return ("オプション2:趣味三昧（ビール飲み放題）")
+
+def future_map_three():
+    return ("ワイのおすすめ:両方！")
+
+future_choice = {
+    "1":future_map_one,
+    "2":future_map_two,
+    "3":future_map_three
+}
+user_input = input("お前の未来を選べ！(1,2,3):")
+
+selected_future = future_choice.get(user_input)
+
+result = selected_future()
+
+print(result)
