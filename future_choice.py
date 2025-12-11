a44da31ba28a21c32d7bda15452b0073dc73109e
KosukeNakagawa95 <kna0891@gmail.com>
2025-12-11 22:47:10 +0900

    refactor: future_choice関数をdictのCHOICESに変更 feat: qを入力で終了

--- future_choice.py
+++ future_choice.py
@@ -1,3 +1,4 @@
+import sys
 # Yeehaw! Optimusで生産性爆上げシミュレーターや！
 human_productivity = 1  # 人間の1年生産性
 optimus_productivity = 5  # Optimusは5倍！
@@ -7,22 +8,43 @@ total_abundance = (human_productivity + (optimus_productivity * num_robots)) * 1
 print(f"Yeehaw! Optimusのおかげで豊かさレベル: {total_abundance}倍！ 働く？ オプションやで！🤠")
 
 # 未来の選択肢関数
-def future_map_one():
-    return ("オプション1: 働く（ボーナスGET）")
+#def future_map_one():
+#    return ("オプション1: 働く（ボーナスGET）")
 
-def future_map_two():
-    return ("オプション2:趣味三昧（ビール飲み放題）")
+#def future_map_two():
+#    return ("オプション2:趣味三昧（ビール飲み放題）")
 
-def future_map_three():
-    return ("ワイのおすすめ:両方！")
+#def future_map_three():
+#    return ("ワイのおすすめ:両方！")
 
-future_choice = {
-    "1":future_map_one,
-    "2":future_map_two,
-    "3":future_map_three
+#future_choice = {
+#    "1":future_map_one,
+#    "2":future_map_two,
+#    "3":future_map_three
+#}
+#user_input = input("お前の未来を選べ！(1,2,3):")
+
+#result = future_choice.get(user_input)
+
+#print(result())
+
+CHOICES = {
+    "1":"オプション1: 働く（ボーナスGET）",
+    "2":"オプション2:趣味三昧（ビール飲み放題）",
+    "3":"ワイのおすすめ:両方！"
 }
-user_input = input("お前の未来を選べ！(1,2,3):")
 
-result = future_choice.get(user_input)
+while True:
+    user_input = input("お前の未来を選べ！（1,2,3):")
 
-print(result())
+    if user_input in CHOICES:
+        print(CHOICES[user_input])
+        while True:
+            user_choide = input("qを押したら終了や！")
+            if user_choide == "q":
+                sys.exit(0)
+
+            else:
+                continue
+    else:
+        print("1,2,3から選べ！テキサス魂を見せろ！")
